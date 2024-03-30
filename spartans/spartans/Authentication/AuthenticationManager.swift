b6dcf08f1ba25f548f8fa68aa09c9107f1f65a94
keertikumarkubareea <keertikumarkubareea@gmail.com>
2024-03-29 21:47:20 -0400

    created a signature of an AuthenticationManager

---
+++ spartans/spartans/Authentication/AuthenticationManager.swift
@@ -0,0 +1,15 @@
+//
+//  AuthenticationManager.swift
+//  spartans
+//
+//  Created by Keertikumar Kubareea on 2024-03-29.
+//
+
+import Foundation
+
+final class AuthenticationManager{
+    
+    static let shared = AuthenticationManager()
+    private init(){}
+    
+}
--- spartans/spartans/Authentication/SignUpView.swift
+++ spartans/spartans/Authentication/SignUpView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+@MainActor
 final class SignUpViewModel : ObservableObject {
     
     @Published var email = ""
